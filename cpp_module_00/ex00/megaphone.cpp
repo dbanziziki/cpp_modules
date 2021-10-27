e620db2736fdf1a34dfef02ab70ebd7273a570c6
David Banziziki <dbanzizi@shi-r11-p1.s19.be>
2021-10-27 16:59:01 +0200

    formated

--- cpp_module_00/ex00/megaphone.cpp
+++ cpp_module_00/ex00/megaphone.cpp
@@ -1,22 +1,19 @@
-#include <iostream>
 #include <cctype>
+#include <iostream>
 
-int main(int argc, char const *argv[])
-{
-	int	i;
+int main(int argc, char const *argv[]) {
+  int i;
 
-	i = 0;
-	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	else
-	{
-		while (++i < argc)
-		{
-			std::string str = argv[i];
-			std::transform(str.begin(), str.end(), str.begin(), toupper);
-			std::cout << str;
-		}
-		std::cout << std::endl;
-	}
-	return 0;
+  i = 0;
+  if (argc == 1)
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+  else {
+    while (++i < argc) {
+      std::string str = argv[i];
+      std::transform(str.begin(), str.end(), str.begin(), toupper);
+      std::cout << str;
+    }
+    std::cout << std::endl;
+  }
+  return 0;
 }
