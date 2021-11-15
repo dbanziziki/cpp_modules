660f7176caf91b0a23a584c406fd45e66e025e18
linzya <alexdavid2@live.fr>
2021-11-15 15:40:29 +0100

    mod8 more tests

--- cpp_module_08/ex02/main.cpp
+++ cpp_module_08/ex02/main.cpp
@@ -14,7 +14,7 @@ int main() {
     mstack.push(3);
     mstack.push(5);
     mstack.push(737);
-    mstack.push(0);
+    mstack.push(19);
     MutantStack<int>::iterator it = mstack.begin();
     MutantStack<int>::iterator ite = mstack.end();
     ++it;
@@ -32,5 +32,6 @@ int main() {
         ++rit;
     }
     std::stack<int> s(mstack);
+    std::cout << "std::stack top: " << s.top() << std::endl;
     return 0;
 }
